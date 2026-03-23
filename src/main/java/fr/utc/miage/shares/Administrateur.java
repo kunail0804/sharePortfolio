b35e445ce5513417c0214b23bd61d3d885e2e8d7
Song Wenkai <wenkais196@gmail.com>
2026-03-23 15:50:38 +0100

    test_us_administrateur- jusque a US12

--- src/main/java/fr/utc/miage/shares/Administrateur.java
+++ src/main/java/fr/utc/miage/shares/Administrateur.java
@@ -1,5 +1,7 @@
 package fr.utc.miage.shares;
 
+import javax.management.relation.Role;
+
 public class Administrateur extends Utilisateur {
 
 
@@ -7,5 +9,37 @@ public class Administrateur extends Utilisateur {
         super(login, password);
         this.setRole(Role.admin);
     }
+
+    public void login(String login, String password) {
+        if (this.getLogin().equals(login) && this.getPassword().equals(password)) {
+            System.out.println("Connexion réussie en tant qu'administrateur.");
+        } else {
+            throw new IllegalArgumentException("Login ou mot de passe incorrect.");
+        }
+    }
+
+    //getters and setters
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    public String getRole() {
+        return this.getRole().toString();
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getLogin() {
+        return this.getLogin();
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return this.getPassword();
+    }
+
+    
     
 }
--- src/test/java/fr/utc/miage/shares/AdministrateurTest.java
+++ src/test/java/fr/utc/miage/shares/AdministrateurTest.java
@@ -1,24 +1,97 @@
 package fr.utc.miage.shares;
 
+import static org.junit.jupiter.api.Assertions.*;
+import fr.utc.miage.shares.Action;
+import fr.utc.miage.shares.ActionSimple;
+import fr.utc.miage.shares.CompositeAction;
+import fr.utc.miage.shares.Administrateur;
+import fr.utc.miage.shares.Jour;
+
 import org.junit.jupiter.api.Test;
 
 public class AdministrateurTest {
+    Administrateur admin = new Administrateur("admin", "admin");
+    Jour jour = new Jour(0, 0);
+    //creation actions composees
+    @Test
+    void testCreateCompositeAction() {
+        ActionSimple action1 = new ActionSimple("Action1");
+        ActionSimple action2 = new ActionSimple("Action2");
+        CompositeAction compositeAction = new CompositeAction("CompositeAction");
+        Jour jour = new Jour(0,0);
+        compositeAction.addAction(action1);
+        compositeAction.addAction(action2);
+        assertEquals(300, compositeAction.valeur(jour));
+    }
+
+    //libelle action
+    @Test
+    void testLibelleAction() {
+        ActionSimple action = new ActionSimple("ActionTest");
+        assertEquals("ActionTest", action.getLibelle());
+    }
+
+    @Test
+    void testLibelleWithInvalidLibelle() {
+        String INVALID_LIBELLE = "";
+        assertThrows(IllegalArgumentException.class, () -> new ActionSimple(INVALID_LIBELLE));
+    }
+
+    @Test
+    void testLibelleWithNullLibelle() {
+        String NULL_LIBELLE = null;
+        assertThrows(IllegalArgumentException.class, () -> new ActionSimple(NULL_LIBELLE));
+    }
+
+    @Test
+    void testLibelleWithLibelleNotUnique() {
+        ActionSimple action1 = new ActionSimple("ActionTest");
+        assertThrows(IllegalArgumentException.class, () -> new ActionSimple(action1.getLibelle()), "Libelle doit être unique");
+    }
+
+    //ajouter les valeurs du jour des actions
+    @Test
+    void testEnregistrerCoursWithNegatifValue() {
+        ActionSimple action = new ActionSimple("ActionTest");
+        Jour jour = new Jour(0, 0);
+        assertThrows(IllegalArgumentException.class, () -> action.enrgCours(jour, -100));
+    }
+
+    @Test
+    void testEnregistrerCoursWithValidValue() {
+        ActionSimple action = new ActionSimple("ActionTest");
+        Jour jour = new Jour(0, 0);
+        assertDoesNotThrow(() -> action.enrgCours(jour, 100));
+    }
+
+    //connection compte administrateur
     @Test
     void testLoginWithValidLoginAndValidPassword() {
-        Administrateur admin = new Administrateur("admin", "admin");
-        assertNotThrows(() -> admin.login("admin", "admin"));
+        assertDoesNotThrow(() -> admin.login("admin", "admin"));
     }
 
     @Test
     void testLoginWithValidLoginAndInvalidPassword() {
-        Administrateur admin = new Administrateur("admin", "admin");
         assertThrows(IllegalArgumentException.class, () -> admin.login("admin", "wrongpassword"));
     }
 
     @Test
     void testLoginWithInvalidLogin() {
-        Administrateur admin = new Administrateur("admin", "admin");
         assertThrows(IllegalArgumentException.class, () -> admin.login("wronglogin", "admin"));
     }
+
+    //Calculer la valeur d'une action composee
+    @Test
+    void testCalculerValeurActionComposee() {
+        ActionSimple action1 = new ActionSimple("Action1");
+        ActionSimple action2 = new ActionSimple("Action2");
+        CompositeAction compositeAction = new CompositeAction("CompositeAction");
+        compositeAction.addAction(action1);
+        compositeAction.addAction(action2);
+        action1.enrgCours(jour, 100);
+        action2.enrgCours(jour, 200);
+        assertEquals(300, compositeAction.valeur(jour));
+    }
+
 
 }
